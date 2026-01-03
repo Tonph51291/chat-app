ebb8afe4ae588645bf3a35456c2bf31553d7fe44
las-tonbd <162237060+Tonph51291@users.noreply.github.com>
2026-01-03 15:00:57 +0700

    add project mobile

---
+++ mobile/components/TextApp.tsx
@@ -0,0 +1,70 @@
+import { Text } from "react-native";
+import React, { memo } from "react";
+import { FlexBoxProp } from "../constants/FlexBoxProp";
+
+interface TextProps extends FlexBoxProp {
+  value: string;
+  color?: string;
+  fontSize?: number;
+  fontFamily?: string;
+  margin?: number;
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+  fontWeight?:
+    | "normal"
+    | "bold"
+    | "100"
+    | "200"
+    | "300"
+    | "400"
+    | "500"
+    | "600"
+    | "700"
+    | "800"
+    | "900"
+    | undefined;
+}
+const TextComponent = (props: TextProps) => {
+  const {
+    value,
+    color,
+    fontFamily,
+    fontSize,
+    marginTop,
+    marginBottom,
+    marginLeft,
+    marginRight,
+    alignSelf,
+    marginVertical,
+    marginHorizontal,
+    margin,
+    fontWeight,
+  } = props;
+  return (
+    <Text
+      {...props}
+      style={[
+        {
+          color: color ?? "black",
+          fontFamily: fontFamily,
+          fontSize: fontSize ?? 14,
+          marginTop: marginTop ?? null,
+          marginBottom: marginBottom ?? null,
+          marginLeft: marginLeft ?? null,
+          marginRight: marginRight ?? null,
+          alignSelf: alignSelf ?? undefined,
+          marginVertical: marginVertical ?? 0,
+          marginHorizontal: marginHorizontal ?? 0,
+          margin: margin ?? 0,
+          fontWeight: fontWeight,
+        },
+      ]}
+    >
+      {value}
+    </Text>
+  );
+};
+
+export default memo(TextComponent);
